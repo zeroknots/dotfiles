2b205b4fab543e45f49b6d3dca112b77b8aa962a
zeroknots <zeroknots@protonmail.com>
2023-09-27 08:11:00 +0700

    rewrite to josh's architecture

---
+++ .config/nvim/lua/core/lazy.lua
@@ -0,0 +1,108 @@
+-- Automatically install lazy
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not vim.loop.fs_stat(lazypath) then
+--   vim.fn.system({
+--     "git",
+--     "clone",
+--     "--filter=blob:none",
+--     "https://github.com/folke/lazy.nvim.git",
+--     "--branch=stable", -- latest stable release
+--     lazypath,
+--   })
+-- end
+-- vim.opt.rtp:prepend(lazypath)
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = " "
+--
+-- local status_ok, lazy = pcall(require, "lazy")
+-- if not status_ok then
+--   return
+-- end
+--
+-- local opts = {
+--   ui = {
+--     size = { width = 0.9, height = 0.9 },
+--     border = "rounded",
+--     title = " Package Manager ",
+--     title_pos = "center",
+--   },
+-- }
+--
+-- local status_plugins_ok, plugins = pcall(require, "core.plugin_list")
+-- if not status_plugins_ok then
+--   return
+-- end
+--
+-- return lazy.setup(plugins, opts)
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.statuscolumn = "%s %l %C"
+vim.cmd([[command! -nargs=0 GoToFile :Telescope find_files]])
+vim.cmd([[command! -nargs=0 GoToCommand :Telescope commands]])
+vim.cmd([[command! -nargs=0 Grep :Telescope live_grep]])
+-- cSpell:word cursorlineopt
+vim.o.cursorlineopt = "number"
+
+vim.g.code_action_menu_show_details = false
+vim.g.code_action_menu_show_diff = true
+vim.g.code_action_menu_show_action_kind = false
+
+require("lazy").setup({
+	spec = {
+		-- add LazyVim and import its plugins
+		-- { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		--
+		-- -- additional plugins
+		-- { import = "lazyvim.plugins.extras.coding.copilot" },
+		-- { import = "lazyvim.plugins.extras.dap.core" },
+		-- { import = "lazyvim.plugins.extras.dap.nlua" },
+		-- { import = "lazyvim.plugins.extras.lang.go" },
+		-- { import = "lazyvim.plugins.extras.lang.json" },
+		-- { import = "lazyvim.plugins.extras.lang.tailwind" },
+		-- { import = "lazyvim.plugins.extras.lang.typescript" },
+		-- { import = "lazyvim.plugins.extras.test.core" },
+		-- -- { import = "lazyvim.plugins.extras.ui.edgy" },
+		-- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+
+		-- import all plugins from the plugins folder
+		{ import = "plugins" },
+	},
+	defaults = {
+		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+		lazy = false,
+		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+		-- have outdated releases, which may break your Neovim install.
+		version = false, -- always use the latest git commit
+		-- version = "*", -- try installing the latest stable version for plugins that support semver
+	},
+	-- cSpell:word tokyonight habamax
+	install = { colorscheme = { "catppuccin", "tokyonight", "habamax" } },
+	ui = {
+		border = "single",
+	},
+	checker = { enabled = true }, -- automatically check for plugin updates
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				-- "matchit",
+				-- "matchparen",
+				-- "netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+})
