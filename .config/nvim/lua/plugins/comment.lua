c3e7a0d256f938b946933761a80497d03c9bcf48
zeroknots <zeroknots@protonmail.com>
2023-09-27 14:01:26 +0700

    comments fixed

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,49 +1,20 @@
 return {
-	"numToStr/Comment.nvim",
-	config = function(_, opts)
-		require("Comment").setup(opts)
-	end,
+	"JoosepAlviste/nvim-ts-context-commentstring",
+	lazy = true,
 	opts = {
-		---Add a space b/w comment and the line
-		padding = true,
-		---Whether the cursor should stay at its position
-		sticky = true,
-		---Lines to be ignored while (un)comment
-		ignore = nil,
-		---LHS of toggle mappings in NORMAL mode
-		toggler = {
-			---Line-comment toggle keymap
-			line = "gcc",
-			---Block-comment toggle keymap
-			block = "gbc",
+		enable = true,
+		enable_autocmd = false,
+	},
+	{
+		"echasnovski/mini.comment",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring()
+						or vim.bo.commentstring
+				end,
+			},
 		},
-		---LHS of operator-pending mappings in NORMAL and VISUAL mode
-		opleader = {
-			---Line-comment keymap
-			line = "gc",
-			---Block-comment keymap
-			block = "gb",
-		},
-		---LHS of extra mappings
-		extra = {
-			---Add comment on the line above
-			above = "gcO",
-			---Add comment on the line below
-			below = "gco",
-			---Add comment at the end of line
-			eol = "gcA",
-		},
-		---Enable keybindings
-		---NOTE: If given `false` then the plugin won't create any mappings
-		mappings = {
-			---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
-			basic = true,
-			---Extra mapping; `gco`, `gcO`, `gcA`
-			extra = true,
-		},
-		---Function to call before (un)comment
-		pre_hook = nil,
-		---Function to call after (un)comment
-		post_hook = nil,
 	},
 }
