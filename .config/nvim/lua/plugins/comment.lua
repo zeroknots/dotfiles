cd544418b3c92aa86b1513c7e1d3dc4dbff9f9e1
Sumer312 <sumergandhi79@gmail.com>
2023-09-27 12:28:09 +0530

    Comment plugin fixed

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,7 +1,20 @@
 return {
-	"numToStr/Comment.nvim",
-  config = function(_,opts)
-    local comment = require("Comment")
-    comment.setup()
-  end,
+	"JoosepAlviste/nvim-ts-context-commentstring",
+	lazy = true,
+	opts = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	{
+		"echasnovski/mini.comment",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				custom_commentstring = function()
+					return require("ts_context_commentstring.internal").calculate_commentstring()
+						or vim.bo.commentstring
+				end,
+			},
+		},
+	},
 }
