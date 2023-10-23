01507509dd44f89b361bc927fac514a0bb2ddcf4
zeroknots <zeroknots@protonmail.com>
2023-10-23 14:31:29 +0700

    changing octo bindings. had a conflict with tmux here

--- .config/nvim/lua/plugins/octo.lua
+++ .config/nvim/lua/plugins/octo.lua
@@ -6,10 +6,23 @@ return {
 		"nvim-telescope/telescope.nvim",
 		"nvim-tree/nvim-web-devicons",
 	},
-	config = function()
-		require("octo").setup({ enable_builtin = true })
+	config = function(_, opts)
+		require("octo").setup(opts)
 		vim.treesitter.language.register("markdown", "octo")
 	end,
+
+	opts = {
+
+		enable_builtin = true,
+		mappings = {
+			submit_win = {
+				approve_review = { lhs = "<space>a", desc = "approve review" },
+				comment_review = { lhs = "<space>c", desc = "comment review" },
+				request_changes = { lhs = "<space>r", desc = "request changes review" },
+				close_review_tab = { lhs = "<space>x", desc = "close review tab" },
+			},
+		},
+	},
 	keys = {
 		{ "<leader>o", "<cmd>Octo<cr>", desc = "Octo" },
 		{ "<leader>oi", "<cmd>Octo issue list<cr>", desc = "Octo list issues" },
